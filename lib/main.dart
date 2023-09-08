67a74f774b2bcbbb9073c49d50f85d4e93dd51e3
AlninoDP <alninodp@gmail.com>
2023-09-08 15:13:08 +0700

    allows scrolling widgets to be dragged by mouse

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:question_task/controller/question_controller.dart';
+import 'package:question_task/custom_scroll_behavoir.dart';
 import 'package:question_task/home_screen.dart';
 
 void main() async {
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         builder: (context, child) {
           return GetMaterialApp(
             title: 'GetX State',
+            scrollBehavior: CustomScrollBehavior(),
             theme: ThemeData(textTheme: GoogleFonts.robotoTextTheme()),
             debugShowCheckedModeBanner: false,
             home: HomeScreen(),
